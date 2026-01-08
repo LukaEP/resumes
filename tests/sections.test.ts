06893c3731a3916fc229104e373582b66a5e58d4
LukaEP <luka.paiva3@gmail.com>
2026-01-08 15:36:15 -0300

    Created Persona Section, extending Section. Need to think about the photo (I hate to work with photos :( )

--- tests/sections.test.ts
+++ tests/sections.test.ts
@@ -1,7 +1,9 @@
 import { describe, expect, test } from "@jest/globals";
 
-import { Section } from "../src/entities/Section";
 import { SectionTypeEnum } from "../src/entities/SectionTypeEnum";
+
+import { Section } from "../src/entities/Section";
+import { PersonaSection } from "../src/entities/sections/Persona";
 
 describe("Generic sections test", () => {
     test("Create generic section", () => {
@@ -28,5 +30,20 @@ describe("Generic sections test", () => {
         let valueReceived = section.getType();
 
         expect(valueReceived).toBe(valueExpected);
+    });
+});
+
+describe("Persona sections test", () => {
+    test("Test if persona section is an instace of a generic section", () => {
+        let valueExpected = Section;
+        let valueReceived = new PersonaSection(SectionTypeEnum.PERSONA,
+            "Bob Tiger",
+            "tiger.bob123@gmail.com",
+            "51912345678",
+            "Street X",
+            "https://linkedin.com/in/bob-tiger-123412312/",
+            "photos/persona/bob_tiger1.png");
+
+        expect(valueReceived).toBeInstanceOf(valueExpected);
     });
 });
