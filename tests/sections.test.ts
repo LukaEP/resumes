2daa88edfb48e83554aeeed6f6b5af57186cf29c
LukaEP <luka.paiva3@gmail.com>
2026-01-08 15:07:15 -0300

    Testing getter and setter of generic section

--- tests/sections.test.ts
+++ tests/sections.test.ts
@@ -5,9 +5,28 @@ import { SectionTypeEnum } from "../src/entities/SectionTypeEnum";
 
 describe("Generic sections test", () => {
     test("Create generic section", () => {
-        let valueReceived = new Section(SectionTypeEnum.PERSONA);
         let valueExpected = Section;
+        let valueReceived = new Section(SectionTypeEnum.PERSONA);
 
         expect(valueReceived).toBeInstanceOf(valueExpected);
+    });
+
+    test("Get section type", () => {
+        let section = new Section(SectionTypeEnum.PERSONA);
+
+        let valueExpected = SectionTypeEnum.PERSONA;
+        let valueReceived = section.getType();
+
+        expect(valueReceived).toBe(valueExpected);
+    });
+
+    test("Set a different type", () => {
+        let section = new Section(SectionTypeEnum.EDUCATION);
+        section.setType(SectionTypeEnum.SKILLS);
+
+        let valueExpected = SectionTypeEnum.SKILLS;
+        let valueReceived = section.getType();
+
+        expect(valueReceived).toBe(valueExpected);
     });
 });
